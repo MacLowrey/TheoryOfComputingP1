be3c41ecf34d4e701906e71bda6ff0e68b808774
Jon Lowrey <jon311456@gmail.com>
2016-09-07 11:56:02 -0500

    Initial Commit

---
+++ p1Theory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p1Theory
+//
+//  Created by Jon Lowrey on 9/7/16.
+//  Copyright © 2016 Jon Lowrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
